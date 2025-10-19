6611dbd99b10a55875558bb664068f2b5355082e
Sam Lucarelli <nuckname13@gmail.com>
2025-10-20 08:39:17 +1100

    Added Colour but doesnt work

--- ProceduralLandmassGeneration/Assets/Scripts/Map Generation/Noise.cs
+++ ProceduralLandmassGeneration/Assets/Scripts/Map Generation/Noise.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public static class Noise {
+
     public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
         float[,] noiseMap = new float[mapWidth,mapHeight];
 
